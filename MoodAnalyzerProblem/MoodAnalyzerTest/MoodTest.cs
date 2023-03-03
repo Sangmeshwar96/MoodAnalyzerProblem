efd589e60291b27d04824db7cd5cd9fd94d08338
Sangmeshwar Ramesh Patil <89923434+Sangmeshwar96@users.noreply.github.com>
2023-03-03 09:23:15 +0530

    [Sangmeshwar] Add: Given “I am in Sad Mood” message in Constructor Should Return SAD

--- MoodAnalyzerProblem/MoodAnalyzerTest/MoodTest.cs
+++ MoodAnalyzerProblem/MoodAnalyzerTest/MoodTest.cs
@@ -5,5 +5,12 @@ namespace MoodAnalyzerTest
 {
     public class MoodTest
     {
+        [Test]
+        public void GivenSadMessage_WhenAnalyze_ShouldReturnSadMood()
+        {
+            MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am in Sad Mood");
+            string result = moodAnalyzer.AnalyzeMood();
+            Assert.AreEqual(result, "Sad");
+        }
     }
 }
