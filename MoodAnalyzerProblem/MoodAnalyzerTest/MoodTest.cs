26d1e9d8bd54e38a3ac297b7f43c87dede04295c
Sangmeshwar Ramesh Patil <89923434+Sangmeshwar96@users.noreply.github.com>
2023-03-03 09:19:53 +0530

    [Sangmeshwar] Refactor: Using Constructor To Result The Message Happy Or Sad

--- MoodAnalyzerProblem/MoodAnalyzerTest/MoodTest.cs
+++ MoodAnalyzerProblem/MoodAnalyzerTest/MoodTest.cs
@@ -5,18 +5,5 @@ namespace MoodAnalyzerTest
 {
     public class MoodTest
     {
-        MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
-        [Test]
-        public void GivenSadMessage_WhenAnalyze_ShouldReturnSadMood()
-        {
-            string result = moodAnalyzer.AnalyzeMood("I am in Sad Mood");
-            Assert.AreEqual(result, "Sad");
-        }
-        [Test]
-        public void GivenSadMessage_WhenAnalyze_ShouldReturnHappyMood()
-        {
-            string result = moodAnalyzer.AnalyzeMood("I am in Happy Mood");
-            Assert.AreEqual(result, "Happy");
-        }
     }
 }
