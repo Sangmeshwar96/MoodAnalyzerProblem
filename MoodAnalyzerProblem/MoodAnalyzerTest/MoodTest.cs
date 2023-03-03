78623ee7ced0d3d1b1f371ba51a0e5e3c2dc3a30
Sangmeshwar Ramesh Patil <89923434+Sangmeshwar96@users.noreply.github.com>
2023-03-03 08:59:15 +0530

    [Sangmeshwar] Add:Given “I am in Any Mood” message Should Return HAPPY

--- MoodAnalyzerProblem/MoodAnalyzerTest/MoodTest.cs
+++ MoodAnalyzerProblem/MoodAnalyzerTest/MoodTest.cs
@@ -5,5 +5,12 @@ namespace MoodAnalyzerTest
 {
     public class MoodTest
     {
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+        [Test]
+        public void GivenSadMessage_WhenAnalyze_ShouldReturnSadMood()
+        {
+            string result = moodAnalyzer.AnalyzeMood("I am in Sad Mood");
+            Assert.AreEqual(result, "Sad");
+        }
     }
 }
